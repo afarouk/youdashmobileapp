31e67af44e367b7801ba2c839deec0f1724a4ab2
weisk <2716537+weisk@users.noreply.github.com>
2021-04-03 09:38:46 +0200

    [wip] missing files

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "extends": ["react-app"],
+  "env": {
+    "browser": true,
+  },
+  "rules": {
+    "react/no-unescaped-entities": 0,
+    "react/display-name": 0,
+    "react/prop-types": 0,
+    "no-unused-vars": 0,
+    "react-hooks/exhaustive-deps": 0,
+    "default-case": 0,
+    "array-callback-return": 0,
+    "import/no-anonymous-default-export": 0,
+    "jsx-a11y/iframe-has-title": 0,
+    "react/jsx-no-target-blank": 0,
+  }
+}
