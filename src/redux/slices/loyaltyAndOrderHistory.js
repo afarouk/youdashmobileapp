e134f6e71b385fa9224b3234a0e5bc42a30a25b8
Oleksandr Oliinyk <best.lessons.new@gmail.com>
2021-05-21 09:25:06 +0200

    fix user orders history

--- src/redux/slices/loyaltyAndOrderHistory.js
+++ src/redux/slices/loyaltyAndOrderHistory.js
@@ -1,10 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+import { userAPI } from '../../services/api';
 
 const initialState = {
   data: null,
   loading: false,
   error: false
 };
+
+export const getLoyaltyAndOrderHistory = createAsyncThunk('loayltyAndOrderHistory/getLoyaltyAndOrderHistory', async (data) => {
+  const response = await userAPI.getLoyaltyAndOrderHistory(data);
+  return response.data;
+});
 
 const loyaltyAndOrderHistorySlice = createSlice({
   name: 'loyaltyAndOrderHistory',
@@ -16,6 +23,22 @@ const loyaltyAndOrderHistorySlice = createSlice({
     resetData: (state, action) => {
       state.data = null;
     }
+  },
+  extraReducers: (builder) => {
+    return builder
+      .addCase(getLoyaltyAndOrderHistory.pending, (state, action) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(getLoyaltyAndOrderHistory.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = false;
+        state.data = action.payload;
+      })
+      .addCase(getLoyaltyAndOrderHistory.rejected, (state, action) => {
+        state.loading = false;
+        state.error = true;
+      })
   }
 });
 export const { setLoyaltyAndOrderHistoryData, resetData } = loyaltyAndOrderHistorySlice.actions;
