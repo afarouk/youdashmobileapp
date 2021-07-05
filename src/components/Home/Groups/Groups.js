d2a7d16c6f95c9c8face52a19519f6434750aa83
Oleksandr Oliinyk <best.lessons.new@gmail.com>
2021-07-05 16:33:34 +0200

    TRELLO-36 update items groups

--- src/components/Home/Groups/Groups.js
+++ src/components/Home/Groups/Groups.js
@@ -1,27 +1,115 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Tabs } from 'antd';
 import stickybits from 'stickybits';
 
 import './Groups.css';
 import { ProductCard } from './ProductCard/ProductCard';
+import { ArrowLeftOutlinedIcon, ArrowRightOutlinedIcon } from "../../Shared/Icons/Icons";
 const { TabPane } = Tabs;
 
 stickybits('.groups-tabs ', { noStyles: true });
 export const Groups = memo(({ groups }) => {
-  const [activeKey, setActiveKey] = useState('1');
-  const handleTabClick = ({ key }) => setActiveKey(key);
-  const resizeTab = groups.length && groups.length < 4;
+  const [activeKey, setActiveKey] = useState(1);
+  const handleTabClick = ([key]) => {
+    setActiveKey(parseInt(key))
+  };
+  const resizeTab = groups.length && groups.length < 3;
+
+  const arrowLeftRef = useRef(null);
+  const arrowRightRef = useRef(null);
+
+  // useEffect(() => {
+  //   if (!groups || !groups.length) {
+  //     return;
+  //   }
+
+  //   const options = {
+  //     root: document.querySelector('.groups-tabs .ant-tabs-nav-wrap'),
+  //     rootMargin: '0px',
+  //     threshold: 1.0
+  //   }
+
+  //   const callback = (entries, observer) => {
+  //     entries.forEach(entry => {
+  //       entry.target.setAttribute('data-is-intersecting', entry.isIntersecting);
+  //     });
+
+  //     // TODO: use refs instead of query selectors
+  //     const tabs = document.querySelectorAll('.groups-tabs .ant-tabs-tab');
+  //     const firstTab = tabs[0];
+  //     const lastTab = tabs[tabs.length - 1];
+
+  //     if (firstTab.getAttribute('data-is-intersecting') === 'true') {
+  //       arrowLeftRef.current.classList.add('disabled')
+  //     } else {
+  //       arrowLeftRef.current.classList.remove('disabled')
+  //     }
+
+  //     if (lastTab.getAttribute('data-is-intersecting') === 'true') {
+  //       arrowRightRef.current.classList.add('disabled')
+  //     } else {
+  //       arrowRightRef.current.classList.remove('disabled')
+
+  //     }
+  //   };
+    
+  //   const observer = new IntersectionObserver(callback, options);
+
+  //   const targets = document.querySelectorAll('.groups-tabs .ant-tabs-tab');
+  //   targets.forEach(target => observer.observe(target));
+
+  //   return () => {
+  //     observer.disconnect();
+  //   }
+
+  // }, [groups])
+
+  const handleLeftClick = (event) => {
+    if (activeKey !== 1) {
+      setActiveKey(activeKey - 1);
+    }
+  }
+
+  const handleRightClick = (event) => {
+    if (activeKey !== groups.length) {
+      setActiveKey(activeKey + 1);
+    }
+  }
+
   return (
     <div className={`groups-tabs ${resizeTab ? `tabs-${groups.length}-only` : ''}`}>
-      <Tabs activeKey={activeKey} onTabClick={handleTabClick}>
-        {(groups || []).map(({ groupDisplayText, id, unSubgroupedItems }, index) => (
-          <TabPane tab={groupDisplayText} key={index + 1}>
-            {(unSubgroupedItems || []).map((product, productIndex) => (
-              <ProductCard product={product} key={`product${index}${productIndex}`} />
-            ))}
-          </TabPane>
-        ))}
+      <Tabs 
+        activeKey={`${activeKey}`} 
+        onTabClick={handleTabClick} 
+        tabBarExtraContent={groups.length > 2 && {
+          left: <ArrowLeftOutlinedIcon 
+            ref={arrowLeftRef} 
+            onClick={handleLeftClick} 
+            className={activeKey === 1 ? 'disabled' : ''}
+          />,
+          right: <ArrowRightOutlinedIcon 
+            ref={arrowRightRef} 
+            onClick={handleRightClick} 
+            className={activeKey === groups.length ? 'disabled' : ''}
+
+          />,
+        }}
+        renderTabBar={(props, DefaultTabBar) => {
+          return <DefaultTabBar {...props} mobile />
+        }}
+      >
+        {(groups || []).map(({ groupDisplayText, id, unSubgroupedItems }, index) => {
+          const title = groupDisplayText.length <= 10 ? groupDisplayText : `${groupDisplayText.substring(0, 10)}...`
+
+          return (
+            <TabPane tab={title} key={index + 1}>
+              {(unSubgroupedItems || []).map((product, productIndex) => (
+                <ProductCard product={product} key={`product${index}${productIndex}`} />
+              ))}
+            </TabPane>
+          )
+        })}
       </Tabs>
     </div>
   );
--- src/components/Shared/Icons/Icons.js
+++ src/components/Shared/Icons/Icons.js
@@ -29,6 +29,45 @@ export const ArrowLeftIcon = (props) => {
     </svg>
   );
 };
+
+export const ArrowLeftOutlinedIcon = React.forwardRef((props, ref) => {
+  return (
+    <svg 
+      viewBox="64 64 896 896" 
+      focusable="false" 
+      data-icon="left" 
+      width="1em" 
+      height="1em" 
+      fill="currentColor" 
+      aria-hidden="true" 
+      ref={ref}
+      {...props} 
+      className={`react-icon ${props.className || ''}`} 
+    >
+      <path d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 000 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z"></path>
+    </svg>
+  );
+});
+
+export const ArrowRightOutlinedIcon = React.forwardRef((props, ref) => {
+  return (
+    <svg 
+      viewBox="64 64 896 896" 
+      focusable="false" 
+      data-icon="right" 
+      width="1em" 
+      height="1em" 
+      fill="currentColor" 
+      aria-hidden="true" 
+      ref={ref}
+      {...props} 
+      className={`react-icon ${props.className || ''}`} 
+    >
+      <path d="M765.7 486.8L314.9 134.7A7.97 7.97 0 00302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 000-50.4z"></path>
+    </svg>
+  );
+});
+
 export const CloseIcon = (props) => {
   return (
     <svg
@@ -310,16 +349,16 @@ export const CheckCircleIcon = (props) => {
 
 export const CheckIcon = (props) => {
   return (
-      <svg
-          className="react-icon"
-          viewBox="0 0 1024 1024"
-          fill="currentColor"
-          height="1em"
-          width="1em"
-          {...props}
-      >
-        <path d="M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 00-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z" />
-      </svg>
+    <svg
+      className="react-icon"
+      viewBox="0 0 1024 1024"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 00-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z" />
+    </svg>
   );
 };
 
@@ -335,4 +374,4 @@ export const ShoppingCartIcon = (props) => {
       <path d="M922.9 701.9H327.4l29.9-60.9 496.8-.9c16.8 0 31.2-12 34.2-28.6l68.8-385.1c1.8-10.1-.9-20.5-7.5-28.4a34.99 34.99 0 00-26.6-12.5l-632-2.1-5.4-25.4c-3.4-16.2-18-28-34.6-28H96.5a35.3 35.3 0 100 70.6h125.9L246 312.8l58.1 281.3-74.8 122.1a34.96 34.96 0 00-3 36.8c6 11.9 18.1 19.4 31.5 19.4h62.8a102.43 102.43 0 00-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7h161.1a102.43 102.43 0 00-20.6 61.7c0 56.6 46 102.6 102.6 102.6s102.6-46 102.6-102.6c0-22.3-7.4-44-20.6-61.7H923c19.4 0 35.3-15.8 35.3-35.3a35.42 35.42 0 00-35.4-35.2zM305.7 253l575.8 1.9-56.4 315.8-452.3.8L305.7 253zm96.9 612.7c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 01-31.6 31.6zm325.1 0c-17.4 0-31.6-14.2-31.6-31.6 0-17.4 14.2-31.6 31.6-31.6s31.6 14.2 31.6 31.6a31.6 31.6 0 01-31.6 31.6z" />
     </svg>
   );
-}
+};
